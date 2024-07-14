5bc86320646bcc80303563f0b8269eb439e2a1e4
subratdev18 <137021154+subratdev18@users.noreply.github.com>
2024-07-14 20:33:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import InvoiceForm from './components/createInvoice/InvoiceForm';
 import { PDFViewer } from '@react-pdf/renderer';
 import InvoicePDF from './components/getPDF/InvoicePDF';
+import Header from './components/header'; 
 
 function App() {
   return (
     <div className="App">
+      <Header /> 
       <InvoiceForm />
     </div>
   );
