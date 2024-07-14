bdb7ddab3d78a704854936ce088121b748136972
subratdev18 <137021154+subratdev18@users.noreply.github.com>
2024-07-14 20:22:00 +0530

    Update InvoiceForm.js

--- src/components/createInvoice/InvoiceForm.js
+++ src/components/createInvoice/InvoiceForm.js
@@ -1,155 +1,239 @@
-import React, { useState } from 'react';
-import InvoicePDF from '../getPDF/InvoicePDF';
-import { PDFDownloadLink } from '@react-pdf/renderer';
-import './styles.css';
+import React,  {  useState }  from 'react'; 
+import {  Input,  InputNumber,  Button,  Alert,  Divider,  DatePicker }  from 'antd'; 
+import dayjs from 'dayjs'; 
+import TableComponent from '../table'; 
+import InvoicePDF from '../getPDF/InvoicePDF'; 
+//  import ShowPreview from '../showPreview'; 
+import {  PDFDownloadLink,  PDFViewer }  from '@react-pdf/renderer'; 
+import './invoiceStyles.css'; 
+ 
+//  import customParseFormat from 'dayjs/plugin/customParseFormat'; 
+//  dayjs.extend(customParseFormat); 
+ 
+const dateFormat = 'DD/MM/YYYY'; 
+ 
+const {  TextArea }  = Input; 
+ 
+const InvoiceForm = () => { 
+  const [logo,  setLogo] = useState('')
+   
+//  items description containing name,  price and quantity 
+  const [items,  setItems] = useState([ 
+    {  key: 0,  item: '',  quantity: 1,  price: 1,  amount: 1 } ,  
+  ]); 
+ 
+//  state for storing info about user creating Invoice 
+  const [billFrom,  setBillFrom] = useState({  
+    name: '', 
+    address: '', 
+    invoiceId: 1, 
+    invoiceDate: dayjs(), 
+    dueDate: dayjs() 
+  } )
 
-const InvoiceForm = () => {
+//  state for capturing info of person who needs to pay 
+  const [client,  setClient] = useState({ 
+    clientName: '', 
+    clientAddress: '', 
+  } )
 
-// state for storing info about user creating Invoice
-  const [billFrom, setBillFrom] = useState({
-    name: '',
-    address: '',
-    invoiceNumber: '',
-  })
-
-// state for capturing info of person who needs to pay
-  const [client, setClient] = useState({
-    clientName: '',
-    clientAddress: '',
-  })
-
-// items description containing name, price and quantity
-  const [items, setItems] = useState([{ name: '', quantity: 0, price: 0}]);
-
-  const handleBillFromData = (e) => {
-    e.preventDefault();
-    const {name, value} = e.target;  
-    setBillFrom({
-      ...billFrom,
+  const handleBillFromData = (e) => { 
+    e.preventDefault(); 
+    const { name,  value}  = e.target;   
+    setBillFrom({ 
+      ...billFrom, 
       [name] : value
-    })
-  }
+    } ) 
+  }  
 
-  const handleClientData = (e) => {
-    e.preventDefault();
-    const {name, value} = e.target;  
-    setClient({
-      ...client,
-      [name] : value
-    })
-  }
+  const handleInvoiceDates = (date,  dateString) => { 
+      console.log("date,  dateString ",  date,  dateString)
 
-  const handleItemChange = (e, index, field, value) => {
-    e.preventDefault();
-    const updatedItems = [...items];
-    updatedItems[index][field] = value; // updating the item field (using index) according to user's input
+      setBillFrom({ 
+        ...billFrom, 
+        [date] : dateString
+      } )
+  } 
 
-    setItems(updatedItems);  // updating the items array
-  };
+  const handleClientData = (e) => { 
+    e.preventDefault(); 
+    const { name,  value}  = e.target;   
+    setClient({ 
+      ...client, 
+      [name] : value 
+    } )
+  } 
 
-  const handleAddItem = () => {
-    setItems([...items, { name: '', quantity: 0, price: 0}]);  // adding new item to items array
-  };
+//  to compute the items' total amount
+  const total = () => { 
+    return items.map(({ price,  quantity} ) => price * quantity).reduce((acc,  currValue) => acc + currValue,  0); 
+  } 
 
-  const handleRemoveItem = (index) => {
-    const updatedItems = [...items];
-    updatedItems.splice(index, 1); // removing the selected item
+  const handleImage = (e) => { 
+    console.log(e.target.files[0])
+    setLogo(e.target.files[0])
 
-    setItems(updatedItems);  // updating the items array 
-  };
+    //  console.log("logo - ",  logo,  logo.length)
 
-// to compute the items' total amount
-  const total = () => {
-    return items.map(({price, quantity}) => price * quantity).reduce((acc, currValue) => acc + currValue, 0);
-  }
+    //  handleUpload functionality
+    const data = new FormData(); 
+    data.append("file",  e.target.files[0])
+    data.append("upload_preset",  "pdfinvoice")
+    data.append("cloud_name",  "dmbqyr2hs")
+
+    fetch("https:// api.cloudinary.com/v1_1/dmbqyr2hs/image/upload", { 
+      method:"post", 
+      body: data
+    } )
+    .then(res => res.json())
+    .then(data => { 
+      console.log(data); 
+      data.url.slice(1,  0); 
+      data.url.slice(0,  -1); 
+      //  console.log("uploadi mage ",  data.url)
+      setLogo(data.url)
+    } )
+    .catch(err => { 
+      showAlert(); 
+      console.log(err)
+    } )
+  } 
+
+  const showAlert = () => { 
+    return <Alert message="Error" type="error" showIcon />
+  } 
 
   return (
-    <div className="invoice">
-      <div>
-        <h1 className='title'>Invoice</h1>
+    <form>
+      <div className="invoice">
+        <div>
+          <h1 className='title'>Invoice</h1>
 
-        <div className='firstRow'>
-          <div className='inputName'>
-            <label>Invoice Number:</label>
-            <input name="invoiceNumber" className="input" type="text" value={billFrom.invoiceNumber} onChange={handleBillFromData} />
+          <div className='uploadContainer'>
+            <input type="file" name="file" accept="image/png,  image/jpeg" onChange={ e => handleImage(e)} />
           </div>
+
+          <div className='invoiceDatesAndNo'>
+            <div className='invoiceDatesAndNoContainer'>
+              <label>Invoice No. :</label>
+              <Input
+                name="invoiceId"
+                className="inputContainer"
+                value={ billFrom.invoiceId}  
+                onChange={ handleBillFromData} 
+              />
+            </div>
+            <div className='invoiceDatesAndNoContainer'>
+              <label>Invoice Date:</label>
+              <DatePicker 
+                name="invoiceDate"
+                className="dateInputContainer"
+                value={ billFrom.invoiceDate}  
+                format={ dateFormat} 
+                onChange={ (date) => handleInvoiceDates("invoiceDate",  date)}  
+              />
+            </div>
+            <div className='invoiceDatesAndNoContainer'>
+              <label>Due Date:</label>
+              <DatePicker 
+                name="dueDate"
+                className="dateInputContainer"
+                //  minDate={ dayjs('14/07/2024',  dateFormat)} 
+                value={ billFrom.dueDate} 
+                format={ dateFormat} 
+                onChange={ (date) => handleInvoiceDates("dueDate",  date)}  
+              />
+            </div>
+          </div>
+
+          <div className='details'>
+            <div className='detailsChild'>
+              <h2>Your Details</h2>
+
+              <div className='inputName'>
+                <label>Name:</label>
+                <Input 
+                  name="name"
+                  className="input" 
+                  value={ billFrom.name}  
+                  onChange={ handleBillFromData} 
+                  allowClear 
+                />
+              </div>
+
+              <div className='inputName'>
+                <label>Address:</label>
+                <TextArea 
+                  name="address" 
+                  className="textarea"
+                  value={ billFrom.address}  
+                  onChange={ handleBillFromData}  
+                  allowClear
+                />
+              </div>
+            </div>
+
+            <Divider type="vertical" />
+
+            <div className='detailsChild'>
+              <h2>Client Details</h2>
+
+              <div className='inputName'>
+                <label>Client Name:</label>
+                <Input 
+                  name="clientName" 
+                  className="input" 
+                  value={ client.clientName}  
+                  onChange={ handleClientData}  
+                  allowClear 
+                />
+              </div>
+
+              <div className='inputName'>
+                <label>Address:</label>
+                <TextArea 
+                  name="clientAddress" 
+                  className="textarea" 
+                  value={ client.clientAddress}  
+                  onChange={ handleClientData} 
+                  allowClear 
+                />
+              </div>
+            
+            </div>
+            
+          </div>
+
+          <hr/>
+
+          <h2 className='title'>Add Details</h2>
+
+          <TableComponent 
+            items={ items}  
+            setItems={ setItems} 
+          />
+
+          <div className='total'>
+            <p>Total:</p>
+            <p>{ total()} </p>
+          </div>
+          <hr/>
+
+          <PDFDownloadLink document={ <InvoicePDF logo={ logo}  billFrom={ billFrom}  client={ client}  total={ total}  items={ items}  />}  fileName={ `Invoice-${ billFrom.invoiceId} .pdf`}  >
+            { ({  blob,  url,  loading,  error } ) =>
+                  loading ? "Loading..." : <Button type="primary" className='button'>Print Invoice</Button>
+                } 
+          </PDFDownloadLink>
         </div>
-
-        <div className='firstRow'>
-          <div className='inputName'>
-            <label>Name:</label>
-            <input name="name" className="input" type="text" value={billFrom.name} onChange={handleBillFromData} />
-          </div>
-          <div className='inputName'>
-            <label>Address:</label>
-            <textarea name="address" className="textarea" type="text" value={billFrom.address} onChange={handleBillFromData} />
-          </div>
-        </div>
-
-        <hr/>
-
-        <h2>Bill To:</h2>
-
-        <div className='firstRow'>
-          <div className='inputName'>
-            <label>Client Name:</label>
-            <input name="clientName" className="input" type="text" value={client.clientName} onChange={handleClientData} />
-          </div>
-          <div className='inputName'>
-            <label>Address:</label>
-            <textarea name="clientAddress" className="textarea" type="text" value={client.clientAddress} onChange={handleClientData} />
-          </div>
-        </div>
-
-        <h2 className='title'>Add Details</h2>
-        <div className='subTitleSection'>
-          <h2 className='subTitle item'>Item</h2>
-          <h2 className='subTitle quantity'>Quantity</h2>
-          <h2 className='subTitle price'>Price</h2>
-          <h2 className='subTitle action'>Amount</h2>
-        </div>
-
-        {items?.map((item, index) => (
-          <div key={index} className='firstRow'>
-            <input className="input item"
-              type="text"
-              value={item.name}
-              onChange={(e) => handleItemChange(e, index, 'name', e.target.value)}
-              placeholder="Item Name"
-            />
-            <input className="input quantity"
-              type="number"
-              value={item.quantity}
-              onChange={(e) => handleItemChange(e, index, 'quantity', e.target.value)}
-              placeholder="Quantity"
-            />
-            <input className="input price"
-              type="number"
-              value={item.price}
-              onChange={(e) => handleItemChange(e, index, 'price', e.target.value)}
-              placeholder="Price"
-            />
-            <p className='amount'>$ {item.quantity * item.price}</p>
-            <button className='button' onClick={() => handleRemoveItem(index)}>-</button>
-          </div>
-        ))}
-        <button className='button' onClick={handleAddItem}>+</button>
-        <hr/>
-
-        <div className='total'>
-          <p>Total:</p>
-          <p>{total()}</p>
-        </div>
-        <hr/>
-
-        <PDFDownloadLink document={<InvoicePDF billFrom={billFrom} client={client} total={total} items={items} />} fileName={"Invoice.pdf"} >
-          {({ blob, url, loading, error }) =>
-                loading ? "Loading..." : <button className='button'>Print Invoice</button>
-              }
-        </PDFDownloadLink>
+      { /* { 
+            <PDFViewer width='800' height={ 800}  showToolbar={ false}  document={ <InvoicePDF billFrom={ billFrom}  client={ client}  total={ total}  items={ items}  />}  >
+                <InvoicePDF billFrom={ billFrom}  client={ client}  total={ total}  items={ items}  />
+            </PDFViewer>
+        }  */} 
       </div>
-    </div>
-  );
-};
+      { /* <ShowPreview billFrom={ billFrom}  client={ client}  items={ items}  total={ total}  /> */} 
+    </form>
+  ); 
+} ; 
 
-export default InvoiceForm;
+export default InvoiceForm; 
