3fb593ca21c51208b3b1e3ad803ce66ee0f95f2a
subratdev18 <137021154+subratdev18@users.noreply.github.com>
2024-07-14 20:30:39 +0530

    Create index.js

---
+++ src/components/header/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>Get Your Invoice in no minutes</div>
+  )
+}
+
+export default Header
