6620c47bff46caf4b9599fe913d8a91a5da9d96a
subratdev18 <137021154+subratdev18@users.noreply.github.com>
2024-10-05 16:42:43 +0530

    add signin button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React, { useState } from 'react';
-import './App.css';
-import InvoiceForm from './components/createInvoice/InvoiceForm';
-import { PDFViewer } from '@react-pdf/renderer';
-import InvoicePDF from './components/getPDF/InvoicePDF';
+import React, {  useState } from 'react'; 
+import './App.css'; 
+import InvoiceForm from './components/createInvoice/InvoiceForm'; 
+// import {  PDFViewer } from '@react-pdf/renderer'; 
+// import InvoicePDF from './components/getPDF/InvoicePDF'; 
 import Header from './components/header'; 
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 
-function App() {
-  return (
-    <div className="App">
-      <Header /> 
-      <InvoiceForm />
-    </div>
-  );
-}
-
-export default App;
+function App() { 
+  return ( 
+    <div className="App"> 
+      <Header>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </Header> 
+      <InvoiceForm /> 
+    </div> 
+  ); 
+} 
+ 
+export default App; 
