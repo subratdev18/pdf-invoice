a27950c8e145257619482fee1e0e4dd21f7bcbfe
subratdev18 <137021154+subratdev18@users.noreply.github.com>
2024-10-05 16:41:21 +0530

    integrate clerk for authentication

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'; 
+import ReactDOM from 'react-dom/client'; 
+import App from './App'; 
+import { ClerkProvider } from '@clerk/clerk-react'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const PUBLISHABLE_KEY = 'pk_test_aGVscGVkLXRlcnJpZXItMzEuY2xlcmsuYWNjb3VudHMuZGV2JA'
+// import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root')); 
+root.render( 
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <App /> 
+    </ClerkProvider>
+  </React.StrictMode> 
+); 
